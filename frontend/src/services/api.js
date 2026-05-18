1ef3d217a217aec98a86e086695ea89d845036d9
antonioaguilar22 <antonioflash22@gmail.com>
2026-05-18 14:50:31 -0600

    fix: baseURL apuntando a Railway en producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://luastock-production.up.railway.app'
 })
 
 export const loginUser = async (email, password) => {
